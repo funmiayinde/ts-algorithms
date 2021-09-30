b9e075fe091f8ee3b22db556b31f0109de087c33
funmiayinde <funmiayinde11@gmail.com>
2021-09-30 04:57:25 +0100

    push, shift and unshift singly linked list

--- datastructure/linked-list/singly-linked-list.ts
+++ datastructure/linked-list/singly-linked-list.ts
@@ -41,6 +41,59 @@ class SinglyLinkedList {
     return this
   }
 
+  pop(): Nodes {
+    if (!this._head) return undefined
+    let current: Nodes = this._head
+    let newTail: Nodes = current
+    while (current.next) {
+      newTail = current
+      current = current.next
+    }
+    this._tail = newTail
+    this._tail.next = null
+    this._length--
+    if (this._length === 0) {
+      this._head = null
+      this._tail = null
+    }
+    return current
+  }
+
+  shift(): Nodes {
+    if (!this._head) return undefined
+    let currentHead: Nodes = this._head
+    this._head = currentHead.next
+    this._length--
+    if (this.length === 0) {
+      this._tail = null
+    }
+    return currentHead
+  }
+
+  unshift(val: any): SinglyLinkedList {
+    let newNode: Nodes = new Nodes(val)
+    if (!this._head) {
+      this._head = newNode
+      this._tail = this._head
+    } else {
+      newNode.next = this._head
+      this._head = newNode
+    }
+    this._length++
+    return this
+  }
+
+  indexOf(index): Nodes {
+    if (index < 0 || index >= this._length) return null
+    let counter = 0
+    let current = this._head
+    while (counter !== index) {
+      current = current.next;
+      counter++
+    }
+    return current
+  }
+
   get head(): Nodes {
     return this._head
   }
@@ -54,7 +107,7 @@ class SinglyLinkedList {
   }
 }
 
-const list = new SinglyLinkedList();
-list.push("Hello");
-list.push("Goodbye");
-console.log(list);
+const list = new SinglyLinkedList()
+list.push('Hello')
+list.push('Goodbye')
+console.log(list.indexOf(0).val);
