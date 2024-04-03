4491e18886906e72c996255b7a77056db2bcb634
funmiayinde <funmiayinde11@gmail.com>
2024-04-03 23:54:10 +0100

    add amazon eazy questions

---
+++ algorithms/amazon/redistribute-characters.ts
@@ -0,0 +1,30 @@
+/**
+ * Given an array of strings words, return true if you can make all strings equal in words array
+ * by performing any number of swap operations. Otherwise, return false.
+ * In one "swap" operation, you can pick any two indices i and j, and move any characters from words[i] to any position of the words[j].
+ * 
+ * Examples
+ * Example 1:
+ * Input: words = ["aaa","bbb","ccc", "abc"]
+ * Expected Output: true
+ * Justification: Each string has 3 characters of a single type, 
+ * and the last string has all 3 characters. We can redistribute the characters to form a string like "abc" making the distribution equal.
+ * 
+ * Example 2:
+ * Input: words = ["ab","bc","cd","de","ea"]
+ * Expected Output: false
+ * Justification: The characters cannot be redistributed to make all strings equal 
+ * because all characters appear only twice, making equal distribution impossible.
+ * 
+ * Example 3:
+ * Input: words = ["zzx","xzz","zxz"]
+ * Expected Output: true
+ * Justification: We can redistribute characters to have each string contain one "x" and two "z"s, like "zzx", "xzz", "zxz", 
+ * maintaining equal distribution.
+ */
+class Solution {
+    makeEqual(words) {
+        // ToDo: Write Your Code Here.
+        return false;
+    }
+}
